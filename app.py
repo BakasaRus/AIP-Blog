0319f25ace286c87ef0238fb6ad0137f9bdf58a7
Nikolay Andreev <bakasaru@list.ru>
2021-02-02 20:10:23 +0300

    Generate articles from server data

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+articles = {
+    1: {
+        'title': 'Основы CSS для бэкенд-разработчиков',
+        'body': 'Равным образом рамки и место обучения кадров позволяет оценить значение форм развития.',
+        'is_new': True
+    },
+    2: {
+        'title': 'Топ-10 CSS-фреймворков',
+        'body': 'Повседневная практика показывает, что постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке дальнейших направлений развития.',
+        'is_new': False
+    },
+}
+
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html', title='Just Another Blog', articles=articles)
 
 
 @app.route('/articles/<int:article_id>')
