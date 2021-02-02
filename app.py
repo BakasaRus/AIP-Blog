df3cc5ea82279f2dfe72fdcd71aa6f614e48e475
Nikolay Andreev <bakasaru@list.ru>
2021-02-02 19:47:00 +0300

    Add index.html as homepage + articles route

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/articles/<int:article_id>')
+def get_article(article_id):
+    return str(article_id)
 
 
 if __name__ == '__main__':
