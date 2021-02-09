f6c5516f57aa3597536badad16b2580d2fe62dee
Nikolay Andreev <bakasaru@list.ru>
2021-02-09 19:45:59 +0300

    Add 404 error handler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -23,7 +23,15 @@ def homepage():
 
 @app.route('/articles/<int:article_id>')
 def get_article(article_id):
-    return render_template('article.html', article=articles[article_id])
+    try:
+        return render_template('article.html', article=articles[article_id])
+    except KeyError:
+        abort(404)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('errors/404.html'), 404
 
 
 if __name__ == '__main__':
