4d03c0b897cdeecc458dd0f1ffe0147f820b498c
Nikolay Andreev <bakasaru@list.ru>
2021-03-02 20:29:02 +0300

    Add Category and Article models

--- models.py
+++ models.py
@@ -9,3 +9,20 @@ class User(db.Model):
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    author = db.relationship('User', backref=db.backref('articles', lazy=True))
+    category = db.relationship('Category', backref=db.backref('articles', lazy=True))
