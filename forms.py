6588a95b4ef28c4b0460d18f577f72ecea5754e4
Nikolay Andreev <bakasaru@list.ru>
2021-03-09 20:44:16 +0300

    Add error display for form

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 from wtforms.widgets import TextArea, TextInput
 
 
 class ArticleForm(FlaskForm):
     title = StringField('Название статьи', validators=[DataRequired()])
     body = StringField('Содержание', validators=[DataRequired()], widget=TextArea())
-    category_id = IntegerField('ID категории', validators=[DataRequired()], widget=TextInput('number'))
-    author_id = IntegerField('ID автора', validators=[DataRequired()], widget=TextInput('number'))
+    category_id = IntegerField('ID категории', validators=[Optional()], widget=TextInput('number'))
+    author_id = IntegerField('ID автора', validators=[Optional()], widget=TextInput('number'))
