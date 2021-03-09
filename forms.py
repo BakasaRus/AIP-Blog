faf8cbf8e184f89df24e2e9ff1fb07de7f5869bf
Nikolay Andreev <bakasaru@list.ru>
2021-03-09 20:40:32 +0300

    Add Bootstrap classes for form

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea, TextInput
 
 
 class ArticleForm(FlaskForm):
     title = StringField('Название статьи', validators=[DataRequired()])
-    body = StringField('Содержание', validators=[DataRequired()])
-    category_id = IntegerField('ID категории', validators=[DataRequired()])
-    author_id = IntegerField('ID автора', validators=[DataRequired()])
+    body = StringField('Содержание', validators=[DataRequired()], widget=TextArea())
+    category_id = IntegerField('ID категории', validators=[DataRequired()], widget=TextInput('number'))
+    author_id = IntegerField('ID автора', validators=[DataRequired()], widget=TextInput('number'))
